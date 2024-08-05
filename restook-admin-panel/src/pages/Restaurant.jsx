d83292604f530838ae5838089e608e01e0f2c56d
Ali Sadeghi <ali909392@gmail.com>
2024-08-05 22:51:55 +0330

    Add base files and dependencies / Create project structure

---
+++ restook-admin-panel/src/pages/Restaurant.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Restaurant = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Restaurant;
---
+++ restook-admin-panel/src/routes/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppRoutes = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AppRoutes;
